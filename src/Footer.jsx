38cab326e8a51cb2fbbeb94041336044308b2743
Antonov-S <anto1nov1@gmail.com>
2024-01-04 01:24:07 +0200

    first commit

---
+++ src/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <small>&copy; Copyright by Svetlozar Antonov. All rights reserved.</small>
+            <small>Last chacked limits: 17 days ago</small>
+        </footer>
+    )
+}
